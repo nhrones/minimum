6702c7dad42e86759e7e2fe439237f778f8537dc
NDH <nick@ndhrones.com>
2023-10-20 23:00:21 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { serveFile } from "https://deno.land/std@0.203.0/http/file_server.ts"
-Deno.serve( handleRequest )
-async function handleRequest(request: Request): Promise<Response> {
+
+Deno.serve( async (request: Request): Promise<Response> => {
    let { pathname } = new URL(request.url);
    if (pathname === '/') pathname = '/index.html';
    console.log('Serving ', pathname)
    const resp = await serveFile(request, '.' + pathname )
    return resp 
-}
+})
